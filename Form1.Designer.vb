2424922615535c624ff40698e92a1666ae3711fe
mgadaphy <mgadaphy1@gmail.com>
2024-06-07 02:35:15 +0100

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,127 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnAdd = New System.Windows.Forms.Button()
+        Me.btnEdit = New System.Windows.Forms.Button()
+        Me.btnDelete = New System.Windows.Forms.Button()
+        Me.lstTasks = New System.Windows.Forms.ListBox()
+        Me.txtTask = New System.Windows.Forms.TextBox()
+        Me.btnSave = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'btnAdd
+        '
+        Me.btnAdd.BackColor = System.Drawing.Color.LawnGreen
+        Me.btnAdd.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.btnAdd.Font = New System.Drawing.Font("Algerian", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnAdd.Location = New System.Drawing.Point(14, 149)
+        Me.btnAdd.Margin = New System.Windows.Forms.Padding(2)
+        Me.btnAdd.Name = "btnAdd"
+        Me.btnAdd.Size = New System.Drawing.Size(76, 32)
+        Me.btnAdd.TabIndex = 0
+        Me.btnAdd.Text = "Add"
+        Me.btnAdd.UseVisualStyleBackColor = False
+        '
+        'btnEdit
+        '
+        Me.btnEdit.BackColor = System.Drawing.Color.DodgerBlue
+        Me.btnEdit.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.btnEdit.Font = New System.Drawing.Font("Algerian", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnEdit.Location = New System.Drawing.Point(117, 149)
+        Me.btnEdit.Margin = New System.Windows.Forms.Padding(2)
+        Me.btnEdit.Name = "btnEdit"
+        Me.btnEdit.Size = New System.Drawing.Size(76, 32)
+        Me.btnEdit.TabIndex = 0
+        Me.btnEdit.Text = "Edit"
+        Me.btnEdit.UseVisualStyleBackColor = False
+        '
+        'btnDelete
+        '
+        Me.btnDelete.BackColor = System.Drawing.Color.Crimson
+        Me.btnDelete.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.btnDelete.Font = New System.Drawing.Font("Algerian", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnDelete.Location = New System.Drawing.Point(212, 149)
+        Me.btnDelete.Margin = New System.Windows.Forms.Padding(2)
+        Me.btnDelete.Name = "btnDelete"
+        Me.btnDelete.Size = New System.Drawing.Size(76, 32)
+        Me.btnDelete.TabIndex = 0
+        Me.btnDelete.Text = "Delete"
+        Me.btnDelete.UseVisualStyleBackColor = False
+        '
+        'lstTasks
+        '
+        Me.lstTasks.Font = New System.Drawing.Font("Times New Roman", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lstTasks.FormattingEnabled = True
+        Me.lstTasks.ItemHeight = 19
+        Me.lstTasks.Location = New System.Drawing.Point(338, 19)
+        Me.lstTasks.Name = "lstTasks"
+        Me.lstTasks.Size = New System.Drawing.Size(250, 251)
+        Me.lstTasks.TabIndex = 1
+        '
+        'txtTask
+        '
+        Me.txtTask.Font = New System.Drawing.Font("Times New Roman", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtTask.Location = New System.Drawing.Point(24, 19)
+        Me.txtTask.Multiline = True
+        Me.txtTask.Name = "txtTask"
+        Me.txtTask.Size = New System.Drawing.Size(297, 91)
+        Me.txtTask.TabIndex = 2
+        '
+        'btnSave
+        '
+        Me.btnSave.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(128, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.btnSave.Font = New System.Drawing.Font("Calibri", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnSave.Location = New System.Drawing.Point(24, 205)
+        Me.btnSave.Name = "btnSave"
+        Me.btnSave.Size = New System.Drawing.Size(85, 30)
+        Me.btnSave.TabIndex = 3
+        Me.btnSave.Text = "Save to File"
+        Me.btnSave.UseVisualStyleBackColor = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(600, 366)
+        Me.Controls.Add(Me.btnSave)
+        Me.Controls.Add(Me.txtTask)
+        Me.Controls.Add(Me.lstTasks)
+        Me.Controls.Add(Me.btnDelete)
+        Me.Controls.Add(Me.btnEdit)
+        Me.Controls.Add(Me.btnAdd)
+        Me.Margin = New System.Windows.Forms.Padding(2)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btnAdd As Button
+    Friend WithEvents btnEdit As Button
+    Friend WithEvents btnDelete As Button
+    Friend WithEvents lstTasks As ListBox
+    Friend WithEvents txtTask As TextBox
+    Friend WithEvents btnSave As Button
+End Class
---
+++ Form1.vb
@@ -0,0 +1,108 @@
+﻿Imports System.IO
+
+Public Class Form1
+    Private toDoItems As New List(Of String)
+    Private filePath As String = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "todolist.txt") ' Default file path
+    Private isEditing As Boolean = False
+
+
+    Private Sub btnEdit_Click(sender As Object, e As EventArgs) Handles btnEdit.Click
+
+        Dim selectedIndex As Integer = lstTasks.SelectedIndex
+
+        If selectedIndex <> -1 Then
+            isEditing = True  'Set editing mode to True
+            txtTask.Text = lstTasks.Items(selectedIndex).ToString()
+
+            btnAdd.Text = "Save"  'Change "Add" button to "Save"
+        End If
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        ' Load tasks from the saved file path (if it exists)
+        If File.Exists(filePath) Then
+            toDoItems.Clear()
+            toDoItems.AddRange(File.ReadAllLines(filePath))
+            lstTasks.Items.AddRange(toDoItems.ToArray())
+        Else
+            ' Initialize with some tasks (optional) - only if the file doesn't exist
+            toDoItems.Add("Buy groceries")
+            toDoItems.Add("Walk the dog")
+            toDoItems.Add("Finish project report")
+            lstTasks.Items.AddRange(toDoItems.ToArray())
+        End If
+
+    End Sub
+
+    Private Sub btnAdd_Click(sender As Object, e As EventArgs) Handles btnAdd.Click
+
+        Dim newTask As String = txtTask.Text.Trim()
+        Dim selectedIndex As Integer = lstTasks.SelectedIndex
+
+        If newTask <> "" Then
+            If isEditing Then
+                ' Update existing task
+                If selectedIndex >= 0 AndAlso selectedIndex < toDoItems.Count Then
+                    toDoItems(selectedIndex) = newTask
+                    lstTasks.Items(selectedIndex) = newTask
+                End If
+                isEditing = False  'Reset editing mode
+                btnAdd.Text = "Add"  'Change back to "Add"
+            Else
+                ' Add new task
+                toDoItems.Add(newTask)
+                lstTasks.Items.Add(newTask)
+            End If
+            txtTask.Clear()
+            lstTasks.SelectedIndex = -1 ' Deselect item after editing or adding
+        End If
+
+    End Sub
+
+    Private Sub lstTasks_DoubleClick(sender As Object, e As EventArgs) Handles lstTasks.DoubleClick
+
+        Dim selectedIndex As Integer = lstTasks.SelectedIndex
+
+        If selectedIndex <> -1 Then
+            isEditing = True  'Set editing mode to True
+            txtTask.Text = lstTasks.Items(selectedIndex).ToString()
+
+            btnAdd.Text = "Save"  'Change "Add" button to "Save"
+        End If
+
+    End Sub
+
+    Private Sub btnDelete_Click(sender As Object, e As EventArgs) Handles btnDelete.Click
+
+        Dim selectedIndex As Integer = lstTasks.SelectedIndex
+
+        If selectedIndex <> -1 Then 'Check if an item is selected
+            toDoItems.RemoveAt(selectedIndex)
+            lstTasks.Items.RemoveAt(selectedIndex)
+            txtTask.Clear() ' Clear the textbox after deleting
+        End If
+
+    End Sub
+
+    Private Sub btnSave_Click(sender As Object, e As EventArgs) Handles btnSave.Click
+
+        Dim saveFileDialog As New SaveFileDialog()
+        saveFileDialog.Filter = "Text Files (*.txt)|*.txt|All Files (*.*)|*.*"
+        saveFileDialog.Title = "Save To-Do List"
+
+        If saveFileDialog.ShowDialog() = DialogResult.OK Then
+            Dim filePath As String = saveFileDialog.FileName
+
+            Using writer As New StreamWriter(filePath)
+                For Each task As String In toDoItems
+                    writer.WriteLine(task)
+                Next
+            End Using
+
+            MessageBox.Show("To-Do list saved successfully!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information)
+        End If
+
+    End Sub
+End Class
